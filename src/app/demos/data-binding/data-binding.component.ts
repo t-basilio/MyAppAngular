dafcbb779d948f8907528917d2730e5a76558df3
Thiago Basilio <basilio.barboza@gmail.com>
2021-11-30 15:09:23 -0300

    Remove lauch.json file

---
+++ src/app/demos/data-binding/data-binding.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html'
+})
+export class DataBindingComponent {
+
+  public contadorClique: number = 0;
+
+  public urlImagem: string = "https://angular.io/assets/images/logos/angular/angular.svg";
+  public nome: string = "";
+
+  mudarImagem(url: string){
+    this.urlImagem = url;
+  }
+
+  adicionarClique(){
+    this.contadorClique++;
+  }
+
+  zerarContador(){
+    this.contadorClique = 0;
+  }
+
+  /* keyUp(event: any){
+     this.nome = event.target.value;
+   } */
+
+ }
+
---
+++ src/app/institucional/sobre/sobre.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html'
+})
+export class SobreComponent {}
+
